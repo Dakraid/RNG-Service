c0bc1ffbbba8ae9d19216616afc0557d548840e3
Kristan Schlikow <kristian@schlikow.de>
2023-03-15 15:18:44 +0100

    Further implementation of the controller

--- RNG.Service/Controllers/DataController.cs
+++ RNG.Service/Controllers/DataController.cs
@@ -1,19 +1,20 @@
-// $SOLUTION$
-// $PROJECT$ / DataController.cs BY Kristian Schlikow
-// First modified on $CREATED_YEAR$.$CREATED_MONTH$.$CREATED_DAY$
-// Last modified on $CURRENT_YEAR$.$CURRENT_MONTH$.$CURRENT_DAY$
+// RNG.Service
+// RNG.Service / DataController.cs BY Kristian Schlikow
+// First modified on 2023.03.12
+// Last modified on 2023.03.13
 
 namespace RNG.Service.Controllers
 {
+#region
     using Database;
 
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.OData.Formatter;
     using Microsoft.AspNetCore.OData.Query;
     using Microsoft.AspNetCore.OData.Results;
-    using Microsoft.AspNetCore.OData.Routing.Controllers;
 
     using Models;
+#endregion
 
     [ApiController]
     [Route("api/[controller]")]
@@ -30,6 +31,10 @@ namespace RNG.Service.Controllers
         }
 
         [EnableQuery(PageSize = 15)]
+        [HttpGet("Get")]
+        public IQueryable<RngEntry> Get() => _context.RngResults.OrderByDescending(p => p.Timestamp).AsQueryable();
+
+        [EnableQuery(PageSize = 15)]
         [HttpGet("GetAllRngAsync")]
         public IQueryable<RngEntry> GetAllRngAsync() => _context.RngResults.OrderByDescending(p => p.Timestamp).AsQueryable();
 
@@ -38,14 +43,15 @@ namespace RNG.Service.Controllers
         public IQueryable<BatchedTest> GetAllTestsAsync() => _context.TestResults.OrderByDescending(p => p.Timestamp).AsQueryable();
 
         [EnableQuery]
-        [HttpGet("GetRngFromDay/{timestamp}")]
+        [HttpGet("GetRngFromDay/{timestamp:datetime}")]
         public IQueryable<RngEntry> GetRngFromDay([FromODataUri] DateTime timestamp) => _context.RngResults.Where(r => r.Timestamp.Date == timestamp.Date);
 
         [EnableQuery]
-        [HttpGet("GetSingleTest/{key}")]
+        [HttpGet("GetSingleTest/{key:int}")]
         public SingleResult<BatchedTest> GetSingleTest([FromODataUri] int key)
         {
             var result = _context.TestResults.Where(b => b.ID == key);
+
             return SingleResult.Create(result);
         }
     }
